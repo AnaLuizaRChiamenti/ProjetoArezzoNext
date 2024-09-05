c46804268a990e05a0c8d37169ab5707dc99552a
AnaLuizaRChiamenti <analuizarodrigueschiamenti@gmail.com>
2024-09-05 11:29:18 -0300

    feat: project text font has been configured

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -10,27 +10,39 @@ export default function Header() {
         <header className="bg-slate-500 w-full h-28 flex items-center justify-between px-20">
             <div className="flex items-center gap-20">
                 <Image alt="arezzo-logo" src={zzlogo} width={100} height={50} />
-                <section className="flex gap-8">
-                    <button>NOVIDADES</button>
-                    <button>SAPATOS</button>
-                    <button>BOTAS</button>
-                    <button>BOLSAS</button>
-                    <button>ACESSÓRIOS</button>
-                    <button>PROMOÇÕES</button>
+                <nav className="flex gap-8">
+                    <button className="btn-hover-effect">NOVIDADES</button>
+                    <button className="btn-hover-effect">SAPATOS</button>
+                    <button className="btn-hover-effect">BOTAS</button>
+                    <button className="btn-hover-effect">BOLSAS</button>
+                    <button className="btn-hover-effect">ACESSÓRIOS</button>
+                    <button className="btn-hover-effect">PROMOÇÕES</button>
                     <div className="w-0.5 h-8 bg-black" />
-                    <button>BRIZZA</button>
-                </section>
+                    <button className="btn-hover-effect">BRIZZA</button>
+                </nav>
             </div>
 
             <section className="flex items-center gap-8">
                 <div className="flex items-center gap-2">
-                    <Image src={icon_local} alt="icone de localização" width={35} height={35} />
-                    <p className="underline ml-2">ATIVE SUA LOCALIZAÇÃO</p>
+                    <button className="flex items-center">
+                        <Image src={icon_local} alt="ícone de localização" width={35} height={35} />
+                        <p className="underline ml-2">ATIVE SUA LOCALIZAÇÃO</p>
+                    </button>
                 </div>
-                <Image src={icon_favorite} alt="icone de favoritos" width={35} height={35} />
-                <Image src={icon_people} alt="icone de perfil" width={33} height={35} />
-                <Image src={icon_bag} alt="icone de sacola" width={35} height={35} />
+
+                <button>
+                    <Image src={icon_favorite} alt="ícone de favoritos" width={35} height={35} />
+                </button>
+
+                <button>
+                    <Image src={icon_people} alt="ícone de perfil" width={33} height={35} />
+                </button>
+
+                <button>
+                    <Image src={icon_bag} alt="ícone de sacola" width={35} height={35} />
+                </button>
             </section>
+
         </header>
     );
 }
